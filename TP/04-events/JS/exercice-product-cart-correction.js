508a578ad9e16ba76370581917664306a031f7a8
Jérôme Poslednik <webmaster@washaweb.com>
2024-06-10 18:27:00 +0200

    events exercice "product cart"

---
+++ TP/04-events/JS/exercice-product-cart-correction.js
@@ -0,0 +1,14 @@
+
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+createApp({
+  data() {
+    return {
+      cart: 0,
+      productImg: 'https://picsum.photos/200/300?image=0',
+    }
+  },
+  // les methodes accessibles dans la vue doivent être déclarées ici :
+  methods: {
+  }
+}).mount('#app')
