1b4d7de5a8dd1f49b36c492924684ed37a688164
Jérôme Poslednik <webmaster@washaweb.com>
2024-06-12 16:06:34 +0200

    fake bill data pour le fomulaire d'édition

--- TP/08-router/08-1-vue-router-basics/src/views/EditBillView.vue
+++ TP/08-router/08-1-vue-router-basics/src/views/EditBillView.vue
@@ -5,16 +5,27 @@
     <!-- 1ère manière de récupérer des paramètres de la route : -->
     <pre>{{ $route.params.id }}</pre>
     <pre>{{ id }}</pre>
+    <pre>{{ bill }}</pre>
   </div>
 </template>
 
 <script>
+import { bills } from '@/seeds/bills.js'
+
 export default {
   props: {
     id: {
       type: [String, Number],
       required: true
     }
+  },
+  data() {
+    return {
+      bill: {}
+    }
+  },
+  mounted() {
+    this.bill = bills.find((bill) => bill.id == this.id)
   }
 }
 </script>
